707fa42fb22386d96585925031f067208ed10cfc
Rakib-208 <ra.hasan3210@gmail.com>
2025-10-13 12:47:42 +0100

    Fix errors

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Myths App",
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Myths App")),
+        body: const Center(child: Text("Welcome to Myths App!")),
+      ),
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_prog/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
