9a73f2996d3de0b80b09cca31c3ceff3a567b4b2
Rakib-208 <ra.hasan3210@gmail.com>
2025-10-22 21:07:40 +0100

    Add add and remove buttons

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,32 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sandwich Shop App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.95,
-            height: 120,
-            color: Colors.blue,
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                OrderItemDisplay(2, 'Club'),
-                OrderItemDisplay(1, 'Footlong'),
-                OrderItemDisplay(3, 'Mini'),
-              ],
-            ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const OrderItemDisplay(5, 'Footlong'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => print('Add button pressed!'),
+                    child: const Text('Add'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => print('Remove button pressed!'),
+                    child: const Text('Remove'),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
@@ -36,22 +40,13 @@ class App extends StatelessWidget {
 }
 
 class OrderItemDisplay extends StatelessWidget {
-  final String itemType;
   final int quantity;
+  final String itemType;
 
   const OrderItemDisplay(this.quantity, this.itemType, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      color: Colors.blue,
-      alignment: Alignment.centerLeft,
-      padding: const EdgeInsets.all(8),
-      child: Text(
-        '$quantity $itemType sandwich(es): ${'🥪' * quantity}',
-        style: const TextStyle(color: Colors.white),
-      ),
-    );
+    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
   }
 }
