24f80ce64a0d6e4a3fc5b475eefd8da3e36f63e2
Rakib-208 <ra.hasan3210@gmail.com>
2025-11-07 23:54:31 +0000

    add Six-inch long sandwich

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sandwich Shop App',
-      home: OrderScreen(maxQuantity: 6),
+      home: OrderScreen(maxQuantity: 5),
     );
   }
 }
@@ -39,6 +39,8 @@ class OrderScreen extends StatefulWidget {
 
 class _OrderScreenState extends State<OrderScreen> {
   int _quantity = 0;
+  int _sixInchQuantity = 0;
+
   void _increaseQuantity() {
     if (_quantity < widget.maxQuantity) {
       setState(() => _quantity++);
@@ -51,10 +53,24 @@ class _OrderScreenState extends State<OrderScreen> {
     }
   }
 
+  void _increaseSixInchQuantity() {
+    if (_sixInchQuantity < widget.maxQuantity) {
+      setState(() => _sixInchQuantity++);
+    }
+  }
+
+  void _decreaseSixInchQuantity() {
+    if (_sixInchQuantity > 0) {
+      setState(() => _sixInchQuantity--);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final isAddDisabled = _quantity >= widget.maxQuantity;
     final isRemoveDisabled = _quantity <= 0;
+    final isSixAddDisabled = _sixInchQuantity >= widget.maxQuantity;
+    final isSixRemoveDisabled = _sixInchQuantity <= 0;
 
     return Scaffold(
       appBar: AppBar(title: const Text('Sandwich Counter')),
@@ -91,6 +107,60 @@ class _OrderScreenState extends State<OrderScreen> {
                 const SizedBox(width: 12),
                 ElevatedButton(
                   onPressed: isRemoveDisabled ? null : _decreaseQuantity,
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.resolveWith<Color>((
+                      states,
+                    ) {
+                      if (states.contains(WidgetState.disabled)) {
+                        return Colors.grey;
+                      }
+                      return Colors.red;
+                    }),
+                    foregroundColor: WidgetStateProperty.resolveWith<Color>((
+                      states,
+                    ) {
+                      if (states.contains(WidgetState.disabled)) {
+                        return Colors.black54;
+                      }
+                      return Colors.white;
+                    }),
+                  ),
+                  child: const Text('Remove'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            OrderItemDisplay(_sixInchQuantity, 'Six-inch'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: isSixAddDisabled ? null : _increaseSixInchQuantity,
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.resolveWith<Color>((
+                      states,
+                    ) {
+                      if (states.contains(WidgetState.disabled)) {
+                        return Colors.grey;
+                      }
+                      return Colors.lightGreenAccent;
+                    }),
+                    foregroundColor: WidgetStateProperty.resolveWith<Color>((
+                      states,
+                    ) {
+                      if (states.contains(WidgetState.disabled)) {
+                        return Colors.black54;
+                      }
+                      return Colors.white;
+                    }),
+                  ),
+                  child: const Text('Add'),
+                ),
+                const SizedBox(width: 12),
+                ElevatedButton(
+                  onPressed: isSixRemoveDisabled
+                      ? null
+                      : _decreaseSixInchQuantity,
                   style: ButtonStyle(
                     backgroundColor: WidgetStateProperty.resolveWith<Color>((
                       states,
